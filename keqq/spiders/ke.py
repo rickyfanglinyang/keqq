578c38ade9f9d5fa11e3d49bc891bf955961639a
Fanglin <yfl0406@gmail.com>
2017-11-02 23:41:43 +0800

    update codes

--- keqq/spiders/ke.py
+++ keqq/spiders/ke.py
@@ -2,7 +2,7 @@
 import scrapy
 import logging
 from scrapy.http import Request
-from lxml import etree
+from keqq.items import KeqqItem
 
 
 
@@ -14,7 +14,7 @@ class KeSpider(scrapy.Spider):
     print("before parse###")
 
     def parse(self, response):
-        print("This is the end of code --------------------------")
+        print("This is the start of response --------------------------")
         # result = response.body.decode('utf-8', 'ignore')
         print(response.xpath("//title/text()").extract())
         # print('what is the result: ',result)
@@ -25,16 +25,16 @@ class KeSpider(scrapy.Spider):
         key = "python"
         for i in range(1,2):
             url = "https://ke.qq.com/course/list/"+str(key)+"?page="+str(i)
-        
+
         yield Request(url=url, callback=self.page)
 
 
     def page(self, response):
         # response.xpath("//h4[@class='item-tt']/a[@class='item-tt-link']/text()").extract()
-        course_name  = response.xpath("//ul[@class='course-card-list']/li[@class='course-card-item']/h4[@class='item-tt']/a[@class='item-tt-link']/text()").extract()
-        sold_count  = response.xpath("//ul[@class='course-card-list']/li[@class='course-card-item']/div[@class='item-line item-line--middle']/span[@class='line-cell item-user']/text()").extract()
-        price  = response.xpath("//ul[@class='course-card-list']/li[@class='course-card-item']/div[@class='item-line item-line--bottom']/span[@class='line-cell item-price']/text()").extract()
-        sold_by  = response.xpath("//ul[@class='course-card-list']/li[@class='course-card-item']/div[@class='item-line item-line--middle']/span[@class='item-source']/a[@class='item-source-link']/text()").extract()
+        # course_name  = response.xpath("//ul[@class='course-card-list']/li[@class='course-card-item']/h4[@class='item-tt']/a[@class='item-tt-link']/text()").extract()
+        # sold_count  = response.xpath("//ul[@class='course-card-list']/li[@class='course-card-item']/div[@class='item-line item-line--middle']/span[@class='line-cell item-user']/text()").extract()
+        # price  = response.xpath("//ul[@class='course-card-list']/li[@class='course-card-item']/div[@class='item-line item-line--bottom']/span[@class='line-cell item-price']/text()").extract()
+        # sold_by  = response.xpath("//ul[@class='course-card-list']/li[@class='course-card-item']/div[@class='item-line item-line--middle']/span[@class='item-source']/a[@class='item-source-link']/text()").extract()
 
         list_courses = []
 
@@ -43,25 +43,42 @@ class KeSpider(scrapy.Spider):
             sold_count  = course.xpath("./div[@class='item-line item-line--middle']/span[@class='line-cell item-user']/text()").extract_first()
             price  = course.xpath("./div[@class='item-line item-line--bottom']/span[@class='line-cell item-price']/text()").extract_first()
             sold_by  = course.xpath("./div[@class='item-line item-line--middle']/span[@class='item-source']/a[@class='item-source-link']/text()").extract_first()
+            link = "https:" + str(course.xpath("./a/@href").extract_first()).strip() # Remove space in front and end of the link
 
+            #For Debug purpose #
             print("course_name ##: ", course_name)
             print("sold_count ##: ", sold_count)
             print("price ##: ", price)
             print("sold_by ##: ", sold_by)
+            print("link ##: ", link)
 
             a_course = []
             a_course.append(course_name)
             a_course.append(sold_count)
             a_course.append(price)
             a_course.append(sold_by)
+            a_course.append(link)
 
             logging.log(logging.WARNING, "####" * 100)
             logging.log(logging.WARNING, a_course)
             logging.log(logging.WARNING, "----" * 100)
 
             list_courses.append(a_course)
+            #For Debug purpose #
+            item = KeqqItem()
+            item["course_name"] = course_name
+            item["sold_count"] = sold_count
+            item["price"] = price
+            item["sold_by"] = sold_by
+            item["link"] = link
+
+
+
+            yield Request(url=link, callback=self.detail)
 
         logging.log(logging.WARNING, "###All Courses###")
-        logging.log(logging.WARNING,list_courses)      
+        logging.log(logging.WARNING,list_courses)
 
-
+    def detail(self, response):
+        title = response.xpath("//title/text()").extract()
+        print("Course Detail Title: ", title)
